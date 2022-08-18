dbf9e8f71ffc03379a34557a80374e912c6a8378
dgregpm <dgregpm@gmail.com>
2022-08-17 23:26:26 -0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCGB7ngb2OpDQJyi-7fzqkf-5IzrQcfhJs",
+    authDomain: "tekflix-1.firebaseapp.com",
+    projectId: "tekflix-1",
+    storageBucket: "tekflix-1.appspot.com",
+    messagingSenderId: "994712342375",
+    appId: "1:994712342375:web:2f43719c50bbbadfcc69c2",
+    measurementId: "G-FVQ3K40TXR"
+  };
