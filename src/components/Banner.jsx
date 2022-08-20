38a7eca09105c49448a88b669bdbe1728e5d0d95
dgregpm <dgregpm@gmail.com>
2022-08-20 14:59:22 -0400

    front-end working

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,27 +1,50 @@
-import React from 'react';
-import './Banner.css'
+import axios from './../axios';
+import React, { useState,useEffect } from 'react';
+import './Banner.css';
+import requests from './../Requests';
 
 function Banner() {
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            //console.log(request);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            )
+            return request;
+        }
+        fetchData();
+        
+    }, []);
 
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n-1) + '...' : string; 
     }
 
+    
+
   return (
     <header className="banner" style={{
         backgroundSize: 'cover' , 
         backgroundPosition: 'center center', 
-        backgroundImage: `url('https://i.imgur.com/e1hLQ2m.png')`
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path || movie?.poster_path}")`,
         }}
     >
     
     <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">{
+            movie?.name || movie?.name || movie?.original_name
+        }</h1>
         <div className="banner__buttons">
             <button className="banner__button">Play</button>
             <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{truncate(`This is a test description`, 10)}</h1>
+        <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
     </div>
 
     <div className="banner--fadeBottom" />
--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import requests from '../Requests';
 import Banner from './Banner';
 import './HomeScreen.css';
 import Nav from './Nav';
+import Row from './Row';
 
 
 function HomeScreen() {
@@ -11,7 +13,18 @@ function HomeScreen() {
           
           <Banner />  
 
-          {/* <Row /> */}    
+          <Row 
+            title='NETFLIX ORIGINALS'
+            fetchUrl={requests.fetchNetflixOriginals}
+            isLargeRow="true"
+          />
+          <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+          <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/> 
+          <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/> 
+          <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/> 
+          <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/> 
+          <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/> 
+          <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/> 
     </div>
   );
 }
